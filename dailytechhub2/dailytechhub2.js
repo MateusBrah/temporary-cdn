41afeac9ba02f8d639e4d14c7fd764c03103ff0f
Mateus Damasceno <103467943+MateusBrah@users.noreply.github.com>
2025-11-25 21:15:21 -0300

    Update dailytechhub2.js

--- dailytechhub2/dailytechhub2.js
+++ dailytechhub2/dailytechhub2.js
@@ -118,7 +118,7 @@ class ModalLoader {
     this.pendingLoads.set(key, []);
 
     // Monta URL do arquivo do modal
-    const url = "https://cdn.jsdelivr.net/gh/MateusBrah/temporary-cdn@ab/dailytechhub2/dailytechhub2.js?v=222123";
+    const url = "https://cdn.jsdelivr.net/gh/MateusBrah/temporary-cdn@ac/dailytechhub2/modals/modal-dailytechhub2-escolha-seu-estilo-de-roupa.js";
 
     // Cria script tag para carregar o modal
     const script = document.createElement("script");
